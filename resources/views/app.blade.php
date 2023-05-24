bcdb736aea64da6943ead9118a7197f9ca924bb9
Ralph Cibis <github@cib.is>
2023-05-24 21:07:34 +0200

    added medium

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,20 +1,21 @@
 <x-head title="ralph cibis" description="culture engineer. organization architect. leadership. modern work. software engineering. climbing. cooking. agile coaching."></x-head>
-<body class="antialiased bg-zinc-100 dark:bg-zinc-800">
+<body class="antialiased bg-zinc-800">
 <div class="flex justify-center items-center h-screen mx-8">
-    <div class="text-left text-black dark:text-zinc-100">
+    <div class="text-left text-zinc-100">
         <h1 class="text-5xl font-bold">ralph cibis</h1>
-        <hr class="border-4 border-green-800 dark:border-green-900 hover:border-amber-400"/>
+        <hr class="border-4 border-green-900"/>
         <div class="">
             <div class="mt-1 font-bold text-xl">
-                culture engineer. organization architect.<br>
-                leadership. modern work. software engineering.<br>
-                climbing. cooking. agile coaching.
+                culture engineer. organization architect.
+                <p class="text-sm">leadership. modern work. software engineering.<br>
+                    climbing. cooking. agile coaching.</p>
             </div>
             <div class="mt-4">
-                <a href="https://agile-punks.com/" target="_blank"><img alt="agile punks website" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 dark:bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/agilepunks.webp')}}"></a>
-                <a href="https://github.com/raphbibus" target="_blank"><img alt="ralph's github profile" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 dark:bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/github.webp')}}"></a>
-                <a href="https://www.linkedin.com/in/ralphcibis/" target="_blank"><img alt="ralph's linkedin profile" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 dark:bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/linkedin.webp')}}"></a>
-                <a href="https://thomann.io" target="_blank"><img alt="thomann.io website" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 dark:bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/thomannio.webp')}}"></a>
+                <a href="https://medium.com/@ralphcibis" target="_blank"><img alt="ralph's medium blog" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 bg-zinc-800 shadow shadow-zinc-800" src="{{asset('img/medium.webp')}}"></a>
+                <a href="https://www.linkedin.com/in/ralphcibis/" target="_blank"><img alt="ralph's linkedin profile" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/linkedin.webp')}}"></a>
+                <a href="https://agile-punks.com/" target="_blank"><img alt="agile punks website" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/agilepunks.webp')}}"></a>
+                <a href="https://github.com/raphbibus" target="_blank"><img alt="ralph's github profile" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/github.webp')}}"></a>
+                <a href="https://thomann.io" target="_blank"><img alt="thomann.io website" class="h-16 sm:h-12 md:h-8 inline rounded-xl mr-1 bg-zinc-100 shadow shadow-zinc-800" src="{{asset('img/thomannio.webp')}}"></a>
             </div>
         </div>
     </div>
